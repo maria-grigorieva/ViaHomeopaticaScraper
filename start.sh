16bd4fbb7747fc7587d512e0c56c3d44cff5636f
Maria Grigorieva <magsend@gmail.com>
2022-07-11 00:28:39 +0300

    bash start script

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# start Celery workers
+nohup /opt/ViaHomeopaticaScraper/venv/vh_py37 -A tasks worker -l info -f logs/celery.log > /dev/null 2>&1&
+echo "Celery Worker started"
+
+# start Celerybeat
+nohup /opt/ViaHomeopaticaScraper/venv/vh_py37 -A tasks beat -l info -f logs/celerybeat.log > /dev/null 2>&1&
+echo "Celerybeat started"
+
+# start Flower
+nohup /opt/ViaHomeopaticaScraper/venv/vh_py37 -A tasks flower -l debug -f logs/flower.log > /dev/null 2>&1&
+echo "Flower started"
